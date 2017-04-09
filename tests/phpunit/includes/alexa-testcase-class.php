b90f66754dedb18bce3267f0cffefbb65ebc152f
Sven Wagener <sven.wagener@rheinschmiede.de>
2017-04-09 10:35:35 +0200

    Moved expectException function

--- tests/phpunit/includes/alexa-testcase-class.php
+++ tests/phpunit/includes/alexa-testcase-class.php
@@ -113,12 +113,4 @@ class Alexa_TestCase extends PHPUnit_Framework_TestCase {
 			}')
 		);
 	}
-
-	public function expectException( $exception ) {
-		if( method_exists( $this, 'setExpectedException' ) ) {
-			return $this->setExpectedException( $exception );
-		}
-
-		return parent::expectException( $exception );
-	}
 }
--- tests/phpunit/includes/phpunit-fallback.php
+++ tests/phpunit/includes/phpunit-fallback.php
@@ -3,5 +3,12 @@ use PHPUnit\Framework\TestCase;
 
 if( ! class_exists( 'PHPUnit_Framework_TestCase' ) ) {
 	class PHPUnit_Framework_TestCase extends TestCase {
+		public function expectException( $exception ) {
+			if( method_exists( $this, 'setExpectedException' ) ) {
+				return $this->setExpectedException( $exception );
+			}
+
+			return parent::expectException( $exception );
+		}
 	}
 }
