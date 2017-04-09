cfa7e4d4acb12a55ec2de82112c44c1ccbd4e8c8
Sven Wagener <sven.wagener@rheinschmiede.de>
2017-04-09 17:05:20 +0200

    Added error reporting

--- tests/phpunit/includes/bootstrap.php
+++ tests/phpunit/includes/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
 require_once dirname( dirname( dirname( dirname( __FILE__ ) ) ) ) . '/src/alexa-sdk.php';
 
 require_once dirname( dirname(__FILE__ ) ) . '/data/test-skill-class.php';
