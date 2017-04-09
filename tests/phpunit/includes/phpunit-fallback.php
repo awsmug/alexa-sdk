9c0b639bb46d4cfac7833e4b615e8e7fe5bd6843
Sven Wagener <sven.wagener@rheinschmiede.de>
2017-04-09 09:55:26 +0200

    Moved expectException function

--- tests/phpunit/includes/phpunit-fallback.php
+++ tests/phpunit/includes/phpunit-fallback.php
@@ -2,13 +2,6 @@
 use PHPUnit\Framework\TestCase;
 
 if( ! class_exists( 'PHPUnit_Framework_TestCase' ) ) {
-	class PHPUnit_Framework_TestCase  extends TestCase {
-		function expectException( $exception ) {
-			if( method_exists( $this, 'setExpectedException' ) ) {
-				return $this->setExpectedException( $exception );
-			} else {
-				return parent::expectException( $exception );
-			}
-		}
+	class PHPUnit_Framework_TestCase extends TestCase {
 	}
 }
