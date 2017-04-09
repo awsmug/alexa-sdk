e69e0ddda0583fc9540d7586e8a62f05f0109b7b
Sven Wagener <sven.wagener@rheinschmiede.de>
2017-04-09 13:11:15 +0200

    Moved expectException function

--- tests/phpunit/includes/alexa-testcase-class.php
+++ tests/phpunit/includes/alexa-testcase-class.php
@@ -113,4 +113,12 @@ class Alexa_TestCase extends PHPUnit_Framework_TestCase {
 			}')
 		);
 	}
+
+	public function expectException( $exception ) {
+		if( method_exists( $this, 'setExpectedException' ) ) {
+			return $this->setExpectedException( $exception );
+		}
+
+		return parent::expectException( $exception );
+	}
 }
