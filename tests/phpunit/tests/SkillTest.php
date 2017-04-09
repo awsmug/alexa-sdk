31a1ae264ec543cbe025e9a27cd5db8a6acb3917
Sven Wagener <sven.wagener@rheinschmiede.de>
2017-04-09 19:03:05 +0200

    Fitted SkillTest functions to actual object

--- tests/phpunit/tests/SkillTest.php
+++ tests/phpunit/tests/SkillTest.php
@@ -3,23 +3,16 @@
 require_once dirname( dirname( __FILE__ ) ) . '/includes/bootstrap.php';
 
 class SkillTest extends Alexa_TestCase {
+	public function setUp() {
+		parent::setUp();
+		$this->skill->input( $this->input[ 'LaunchRequest' ] );
+	}
+
 	public function testInput(){
-		$input = $this->skill->input( $this->input[ 'LaunchRequest' ] );
-		$this->assertInstanceOf( 'Alexa\Input\Input_Stream', $input );
+		$this->assertInstanceOf( 'Alexa\Input\Input_Stream', $this->skill->input() );
 	}
 
-	public function testRequest(){
-		$input = $this->skill->input( $this->input[ 'LaunchRequest' ] );
-		$this->assertInstanceOf( 'Alexa\Input\Request', $input->request() );
-	}
-
-	public function testSession(){
-		$input = $this->skill->input( $this->input[ 'LaunchRequest' ] );
-		$this->assertInstanceOf( 'Alexa\Input\Session', $input->session() );
-	}
-
-	public function testVersion(){
-		$input = $this->skill->input( $this->input[ 'LaunchRequest' ] );
-		$this->assertEquals( '1.0', $input->get_version() );
+	public function testOutput() {
+		$this->assertInstanceOf( 'Alexa\Output\Output_Stream', $this->skill->output() );
 	}
 }
