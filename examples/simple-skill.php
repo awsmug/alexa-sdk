a0659cde460e2acd869a037f3c303e5bbc9d7d00
Sven Wagener <sven.wagener@rheinschmiede.de>
2017-04-12 23:02:03 +0200

    Added first version of an example

---
+++ examples/simple-skill.php
@@ -0,0 +1,34 @@
+<?php
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
+require_once dirname( dirname( __FILE__ ) ) . '/src/alexa-sdk.php';
+
+use Alexa\Skill_Template;
+use Alexa\Exception;
+
+class Simple_Skill extends Skill_Template {
+
+	public function intent_request() {
+		/**
+		 * Put in your answer stuff here!
+		 */
+		$this->output()->response()->output_speech()->set_text( 'You started the skill!' );
+
+		$this->output()->response()->card()->set_title( 'Simple Skill card');
+		$this->output()->response()->card()->set_text('This is a dummy Text for my Skill Card.');
+
+		$this->output()->response()->end_session();
+	}
+}
+
+$simple_skill = new Simple_Skill( 'amzn1.ask.skill.f66f9cb9-c632-42bb-be50-210f1d6164b6' );
+
+try{
+	$simple_skill->run();
+} catch( Exception $exception) {
+	$simple_skill->log( $exception->getMessage() );
+	echo $exception->getMessage();
+}
